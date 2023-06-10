23da68d0a63cac8d54e199fdd347bd749bbc0c31
Ramanarayana <gunjirama1999@gmail.com>
2023-06-10 22:55:18 +0530

    Terraform_list of concepts and variable

--- 01.vars/vars.tf
+++ 01.vars/vars.tf
@@ -15,7 +15,7 @@ output "sample_string" {
 ## above case we are printing just variable combination of some strings then following is the syntax.output
 ## ${} is compulsory f we include the variable inside a string
 
-output "sample_string" {
+output "sample_string1" {
   value ="value of sample string =${var.sample_string}"
 
 }
