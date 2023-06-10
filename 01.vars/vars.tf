e0b4a84f1a4209e51816e3cc8e46a8e51213de88
Ramanarayana <gunjirama1999@gmail.com>
2023-06-10 22:50:31 +0530

    Terraform_list of concepts and variable

---
+++ 01.vars/vars.tf
@@ -0,0 +1,21 @@
+## the way of declaring a variables
+variable "sample_string" {
+  default = "Hello World"
+}
+
+## Shell scripting equivalent sample_string =" Hello World"
+
+## To print in the shell scripting, We use echo, following is the syntax in shell
+## scho $sample_string
+
+output "sample_string" {
+   value =var.sample_string
+}
+
+## above case we are printing just variable combination of some strings then following is the syntax.output
+## ${} is compulsory f we include the variable inside a string
+
+output "sample_string" {
+  value ="value of sample string =${var.sample_string}"
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # learn-terraform
+
+1. Variables
+2. Functions
+3. Data Sources
+4. Resources 
+5. Module
+6. Locals
+7. Providers
+8. State Files
+9. Loops
+10. Conditions
+
