69da033e5c76b0c99c163ac13a7688858c5c3cda
Ramanarayana <gunjirama1999@gmail.com>
2023-06-13 16:41:27 +0530

    Terraform_list

---
+++ 01.vars/outputs.tf
@@ -0,0 +1,20 @@
+## To print in the shell scripting, We use echo, following is the syntax in shell
+## echo $sample_string
+
+output "sample_string" {
+   value =var.sample_string
+}
+
+## above case we are printing just variable combination of some strings then following is the syntax.output
+## ${} is compulsory f we include the variable inside a string
+
+output "sample_string1" {
+  value ="value of sample string =${var.sample_string}"
+
+}
+
+## values from list
+# second value in list
+output "sample_list_2" {
+value = var.sample_list[1]
+}
--- 01.vars/vars.tf
+++ 01.vars/vars.tf
@@ -13,4 +13,29 @@ output "sample_string2" {
   value ="My name is =${var.sample_string_rama1}"
 
 }
+## 10.6 is also a number in terraform
+## for creating number and boolean data types:
 
+variable "sample_number" {
+  default =100
+ }
+
+variable "sample_boolean" {
+ default =true
+}
+
+## booleans are true or false
+# Booleans and numbers does not need to be quoted, only strings need to be quoted that to
+# be with double quotes.Terraform does support for single quotes.
+
+### variable types
+# So far we have seen is default variable types#
+
+# We have list variable types, Meaning single variable can hold multiple values
+variable "sample_list" {
+ default = [
+ 100,"hello",true]
+
+}
+
+# data type inside the list does not matter in terraform
