eda073c992eb48d57d8acf150e224c339ea46733
Ramanarayana <gunjirama1999@gmail.com>
2023-06-13 16:25:00 +0530

    Terraform_list of concepts and variable

--- 01.vars/vars.tf
+++ 01.vars/vars.tf
@@ -5,21 +5,6 @@ variable "sample_string" {
 
 ## Shell scripting equivalent sample_string =" Hello World"
 
-## To print in the shell scripting, We use echo, following is the syntax in shell
-## scho $sample_string
-
-output "sample_string" {
-   value =var.sample_string
-}
-
-## above case we are printing just variable combination of some strings then following is the syntax.output
-## ${} is compulsory f we include the variable inside a string
-
-output "sample_string1" {
-  value ="value of sample string =${var.sample_string}"
-
-}
-
 variable "sample_string_rama1" {
   default = "Ramanarayana"
 }
