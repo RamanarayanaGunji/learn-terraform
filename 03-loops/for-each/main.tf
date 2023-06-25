93f4771ad655aa5cd80ec37fd21c744ad1c1db9d
Ramanarayana <gunjirama1999@gmail.com>
2023-06-25 12:12:37 +0530

    Terraform_for_each_count print key-values_v5

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -7,13 +7,42 @@ provisioner "local-exec" {
     command ="echo Fruit Name - ${each.key} -${each.value}"
    // ## prints number of items are giving, here out put is 3( three)
    // #command = "echo ${length(var.fruits)}"
+}
+}
 
+resource "null_resource" "fruits1" {
+for_each =var.fruits1
+
+provisioner "local-exec" {
+    command ="echo Fruit1 Name - ${each.key} -${each.value}"
+   // ## prints number of items are giving, here out put is 3( three)
+   // #command = "echo ${length(var.fruits)}"
 }
 }
+
 variable "fruits" {
    default = {
    apple =100
    orange =2000
    banana =1000
   }
+}
+
+variable "fruits1" {
+   default ={
+    apple ={
+     name ="apple"
+     count =58
+    }
+    orange ={
+      name ="orange"
+      count =24
+     }
+    apple ={
+      name ="banana"
+      count =12
+    }
+
+   }
+
 }
