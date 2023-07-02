9de19e8dc12d5da83decdc92e556b55ca01ed4c0
Ramanarayana <gunjirama1999@gmail.com>
2023-07-03 00:20:26 +0530

    Learn-terraform_functions_Collections/elements_V3

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -7,3 +7,12 @@ default ="abcxyz"
 output "sample" {
 value =upper(var.sample)
 }
+
+variable "sample1" {
+default =["abc","xyz"]
+}
+
+## To print first value like var.sample1[0] with function  here we are using elements
+ output "sample1" {
+ value =element(var.sample1,0)
+ }
